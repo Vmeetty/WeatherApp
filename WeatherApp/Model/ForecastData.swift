b0dc0d557490a105d3834d89c868df41b9fa1937
Vlad Ch <vividimagelogin@gmail.com>
2022-05-27 18:08:28 +0300

    -

---
+++ WeatherApp/Model/ForecastData.swift
@@ -0,0 +1,28 @@
+//
+//  ForecastData.swift
+//  WeatherApp
+//
+//  Created by user on 27.05.2022.
+//
+
+import Foundation
+
+
+struct ForecastData: Codable {
+    
+    let list: [Forecast]
+    
+}
+
+struct Forecast: Codable {
+    
+    let temp: Temp
+    let weather: [Weather]
+    
+}
+
+struct Temp: Codable {
+    
+    let day: Double
+    
+}
